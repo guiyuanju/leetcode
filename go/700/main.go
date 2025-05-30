a15d87b49edd99fd9dafd282ebda044e4b57f0eb
guiyuanju <guiyuanjune@outlook.com>
2025-05-30 13:01:12 +0800

    add 700

---
+++ go/700/main.go
@@ -0,0 +1,100 @@
+package main
+
+import "fmt"
+
+func main() {
+	root := makeBinaryTree([]any{4, 2, 7, 1, 3})
+	val := 2
+	printBinaryTree(searchBST(root, val))
+
+	root = makeBinaryTree([]any{4, 2, 7, 1, 3})
+	val = 5
+	printBinaryTree(searchBST(root, val))
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	cur := root
+	for cur != nil {
+		if cur.Val == val {
+			return cur
+		} else if cur.Val > val {
+			cur = cur.Left
+		} else {
+			cur = cur.Right
+		}
+	}
+	return nil
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func makeBinaryTree(nodes []any) *TreeNode {
+	var root *TreeNode
+	queue := []*TreeNode{}
+	for i, n := range nodes {
+		var cur *TreeNode
+		if n != nil {
+			cur = &TreeNode{
+				Val:   n.(int),
+				Left:  nil,
+				Right: nil,
+			}
+		}
+		queue = append(queue, cur)
+
+		for queue[0] == nil {
+			queue = queue[1:]
+		}
+
+		if i == 0 {
+			root = cur
+		} else if i%2 != 0 {
+			queue[0].Left = cur
+		} else {
+			queue[0].Right = cur
+			queue = queue[1:]
+		}
+	}
+	return root
+}
+
+func printBinaryTree(root *TreeNode) {
+	flat := []*TreeNode{}
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+		flat = append(flat, cur)
+		if cur != nil {
+			queue = append(queue, cur.Left)
+			queue = append(queue, cur.Right)
+		}
+	}
+	for i := len(flat) - 1; i >= 0 && flat[i] == nil; i-- {
+		flat = flat[:len(flat)-1]
+	}
+	fmt.Print("[")
+	for i, v := range flat {
+		if v != nil {
+			fmt.Print(v.Val)
+		} else {
+			fmt.Print("null")
+		}
+		if i < len(flat)-1 {
+			fmt.Print(", ")
+		}
+	}
+	fmt.Println("]")
+}
+
+func assertEq[T comparable](a, b T) {
+	if a != b {
+		fmt.Printf("Failed: %v != %v\n", a, b)
+	} else {
+		fmt.Printf("Ok: %v\n", a)
+	}
+}
