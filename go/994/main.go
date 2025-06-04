2a40268e5a3ba9da13aab9f0a46ea8ea9b9c2c84
guiyuanju <guiyuanjune@outlook.com>
2025-06-04 14:45:24 +0800

    update 994

--- go/994/main.go
+++ go/994/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
@@ -25,65 +24,49 @@ func assertEq[T comparable](a, b T) {
 }
 
 func orangesRotting(grid [][]int) int {
-	M := len(grid)
-	N := len(grid[0])
-	minutes := make([][]int, M)
-	for i := range M {
-		minutes[i] = make([]int, N)
-		for j := range N {
-			minutes[i][j] = math.MaxInt
-		}
-	}
-
 	type step struct {
 		row, col, step int
 	}
 
+	M := len(grid)
+	N := len(grid[0])
+
+	seen := map[[2]int]bool{}
+	queue := []step{}
+	freshCount := 0
+	for i := range M {
+		for j := range N {
+			if grid[i][j] == 2 {
+				queue = append(queue, step{i, j, 0})
+				seen[[2]int{i, j}] = true
+			} else if grid[i][j] == 1 {
+				freshCount++
+			}
+		}
+	}
+
+	directions := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
 	valid := func(row, col int) bool {
 		return 0 <= row && row < M && 0 <= col && col < N && grid[row][col] == 1
 	}
 
-	directions := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
-
-	bfs := func(row, col int) {
-		seen := map[[2]int]bool{}
-		seen[[2]int{row, col}] = true
-		queue := []step{{row, col, 0}}
-		for len(queue) > 0 {
-			cur := queue[0]
-			queue = queue[1:]
-			for _, dir := range directions {
-				newRow, newCol := cur.row+dir[0], cur.col+dir[1]
-				if !seen[[2]int{newRow, newCol}] && valid(newRow, newCol) {
-					seen[[2]int{newRow, newCol}] = true
-					if minutes[newRow][newCol] <= cur.step+1 {
-						continue
-					}
-					minutes[newRow][newCol] = cur.step + 1
-					queue = append(queue, step{newRow, newCol, cur.step + 1})
-				}
-			}
-		}
-	}
-
-	for i := range M {
-		for j := range N {
-			if grid[i][j] == 2 {
-				bfs(i, j)
-			}
-		}
-	}
-
 	var res int
-	for i := range M {
-		for j := range N {
-			if grid[i][j] == 1 && minutes[i][j] == math.MaxInt {
-				return -1
-			}
-			if minutes[i][j] != math.MaxInt {
-				res = max(res, minutes[i][j])
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+		for _, dir := range directions {
+			newRow, newCol := cur.row+dir[0], cur.col+dir[1]
+			if !seen[[2]int{newRow, newCol}] && valid(newRow, newCol) {
+				seen[[2]int{newRow, newCol}] = true
+				queue = append(queue, step{newRow, newCol, cur.step + 1})
+				res = max(res, cur.step+1)
+				freshCount--
 			}
 		}
+	}
+
+	if freshCount > 0 {
+		return -1
 	}
 	return res
 }
