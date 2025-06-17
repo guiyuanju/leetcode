63278de75c18106b63eece5af863c780fb80fa40
guiyuanju <guiyuanjune@outlook.com>
2025-06-17 15:24:29 +0800

    add 881

---
+++ go/881/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	people := []int{1, 2}
+	limit := 3
+	assertEq(1, numRescueBoats(people, limit))
+
+	people = []int{3, 2, 2, 1}
+	limit = 3
+	assertEq(3, numRescueBoats(people, limit))
+
+	people = []int{3, 5, 3, 4}
+	limit = 5
+	assertEq(4, numRescueBoats(people, limit))
+
+	people = []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4}
+	limit = 4
+	assertEq(8, numRescueBoats(people, limit))
+}
+
+func assertEq[T comparable](a, b T) {
+	if a != b {
+		fmt.Printf("Failed: %v != %v\n", a, b)
+	} else {
+		fmt.Printf("Ok: %v\n", a)
+	}
+}
+
+func numRescueBoats(people []int, limit int) int {
+	slices.SortFunc(people, func(a, b int) int { return b - a })
+	i := 0
+	j := len(people) - 1
+	var res int
+	for i <= j {
+		if people[i]+people[j] <= limit {
+			j--
+		}
+		i++
+		res++
+	}
+	return res
+}
