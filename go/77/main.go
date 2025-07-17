e05b4e980c81880f0b323231aaa316aad5d7d2db
guiyuanju <guiyuanjune@outlook.com>
2025-07-17 16:10:57 +0800

    Update main.go

--- go/77/main.go
+++ go/77/main.go
@@ -1,4 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(combine(4, 2))
+	fmt.Println(combine(1, 1))
+}
+
+func combine(n int, k int) [][]int {
+	var res [][]int
+	var bt func(cur []int, i int)
+	bt = func(cur []int, i int) {
+		if len(cur) == k {
+			res = append(res, append([]int(nil), cur...))
+			return
+		}
+		for j := i; j <= n; j++ {
+			cur = append(cur, j)
+			bt(cur, j+1)
+			cur = cur[:len(cur)-1]
+		}
+	}
+	bt(nil, 1)
+	return res
 }
