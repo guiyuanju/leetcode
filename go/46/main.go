17b3225e9dc42d99a6c52fead279199be1c34441
guiyuanju <guiyuanjune@outlook.com>
2025-07-17 13:00:14 +0800

    add 46

---
+++ go/46/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	nums := []int{1, 2, 3}
+	fmt.Println(permute(nums))
+
+	nums = []int{0, 1}
+	fmt.Println(permute(nums))
+
+	nums = []int{1}
+	fmt.Println(permute(nums))
+}
+
+func permute(nums []int) [][]int {
+	var res [][]int
+	var bt func([]int)
+	bt = func(cur []int) {
+		if len(cur) == len(nums) {
+			res = append(res, append([]int(nil), cur...))
+			return
+		}
+		for _, n := range nums {
+			if !slices.Contains(cur, n) {
+				bt(append(cur, n))
+			}
+		}
+	}
+	bt(nil)
+	return res
+}
