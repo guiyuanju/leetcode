5e58bf8ddcea9ffe56541fbfb1825d4707c97466
guiyuanju <guiyuanjune@outlook.com>
2025-08-19 10:47:39 +0800

    add 523

--- go/523/go.mod
+++ go/523/go.mod
@@ -1,3 +1,5 @@
 module 523
 
 go 1.24.3
+
+require github.com/guiyuanju/lcutils v0.0.4
--- go/523/main.go
+++ go/523/main.go
@@ -1,34 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"github.com/guiyuanju/lcutils/assert"
+)
 
 func main() {
-	fmt.Println(checkSubarraySum([]int{23, 2, 4, 6, 7}, 6))
-	fmt.Println(checkSubarraySum([]int{23, 2, 6, 4, 7}, 6))
-	fmt.Println(checkSubarraySum([]int{23, 2, 6, 4, 7}, 13))
-	fmt.Println(checkSubarraySum([]int{1, 0}, 2))
-	fmt.Println(checkSubarraySum([]int{5, 0, 0, 0}, 3))
+	assert.Eq(true, checkSubarraySum([]int{23, 2, 4, 6, 7}, 6))
+	assert.Eq(true, checkSubarraySum([]int{23, 2, 6, 4, 7}, 6))
+	assert.Eq(false, checkSubarraySum([]int{23, 2, 6, 4, 7}, 13))
+	assert.Eq(false, checkSubarraySum([]int{1, 0}, 2))
+	assert.Eq(true, checkSubarraySum([]int{5, 0, 0, 0}, 3))
+	assert.Eq(false, checkSubarraySum([]int{1, 2, 12}, 6))
 }
 
+// (cur-sum)%k == 0
+// cur%k - sum%k == 0
+// sum%k == cur%k
 func checkSubarraySum(nums []int, k int) bool {
 	if len(nums) < 2 {
 		return false
 	}
 
-	m := map[int]int{}
-	m[0] = -1
-
+	sums := map[int]int{}
+	sums[0] = -1
 	var cur int
-	for i, n := range nums {
-		cur += n
-		if idx, ok := m[cur%k]; ok {
-			if i-idx > 1 {
+	for j := 0; j < len(nums); j++ {
+		cur += nums[j]
+		if v, ok := sums[cur%k]; ok {
+			if j-v > 1 {
 				return true
 			}
 		} else {
-			m[cur%k] = i
+			sums[cur%k] = j
 		}
 	}
-
 	return false
 }
