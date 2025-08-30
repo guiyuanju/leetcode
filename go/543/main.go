201238352b1d9e5024b76c5ca828cfaf0efca9ac
guiyuanju <guiyuanjune@outlook.com>
2025-08-30 15:49:10 +0800

    add 543

---
+++ go/543/main.go
@@ -0,0 +1,98 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(diameterOfBinaryTree(makeBinaryTree([]any{1, 2, 3, 4, 5})))
+	fmt.Println(diameterOfBinaryTree(makeBinaryTree([]any{1, 2})))
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func diameterOfBinaryTree(root *TreeNode) int {
+	var dfs func(*TreeNode) (int, int)
+	dfs = func(root *TreeNode) (int, int) {
+		if root == nil {
+			return 0, 0
+		}
+
+		leftDepth, leftLen := dfs(root.Left)
+		rightDepth, rightLen := dfs(root.Right)
+
+		return 1 + max(leftDepth, rightDepth), max(leftLen, rightLen, leftDepth+rightDepth)
+	}
+
+	_, res := dfs(root)
+	return res
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func makeBinaryTree(nodes []any) *TreeNode {
+	var root *TreeNode
+	queue := []*TreeNode{}
+	for i, n := range nodes {
+		var cur *TreeNode
+		if n != nil {
+			cur = &TreeNode{
+				Val:   n.(int),
+				Left:  nil,
+				Right: nil,
+			}
+		}
+		queue = append(queue, cur)
+
+		for queue[0] == nil {
+			queue = queue[1:]
+		}
+
+		if i == 0 {
+			root = cur
+		} else if i%2 != 0 {
+			queue[0].Left = cur
+		} else {
+			queue[0].Right = cur
+			queue = queue[1:]
+		}
+	}
+	return root
+}
+
+func printBinaryTree(root *TreeNode) {
+	flat := []*TreeNode{}
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+		flat = append(flat, cur)
+		if cur != nil {
+			queue = append(queue, cur.Left)
+			queue = append(queue, cur.Right)
+		}
+	}
+	for i := len(flat) - 1; i >= 0 && flat[i] == nil; i-- {
+		flat = flat[:len(flat)-1]
+	}
+	fmt.Print("[")
+	for i, v := range flat {
+		if v != nil {
+			fmt.Print(v.Val)
+		} else {
+			fmt.Print("null")
+		}
+		if i < len(flat)-1 {
+			fmt.Print(", ")
+		}
+	}
+	fmt.Println("]")
+}
