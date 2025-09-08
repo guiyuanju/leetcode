41a85f2ed441bd038aa73bf63f9648a2a3f477a5
guiyuanju <guiyuanjune@outlook.com>
2025-09-08 11:41:00 +0800

    upd 2182

--- go/2182/main.go
+++ go/2182/main.go
@@ -1,7 +1,6 @@
 package main
 
 import "fmt"
-import "container/heap"
 
 func main() {
 	s := "cczazcc"
@@ -26,54 +25,36 @@ func assertEq[T comparable](a, b T) {
 }
 
 func repeatLimitedString(s string, repeatLimit int) string {
-	count := map[byte]int{}
-	for _, r := range s {
-		c := byte(r)
-		count[c]++
+	count := [26]int{}
+	for _, c := range []byte(s) {
+		count[c-'a']++
 	}
 
-	h := new(Heap)
-	for k, v := range count {
-		heap.Push(h, &Item{k, v})
-	}
-
-	res := make([]byte, 0, len(s))
-	for h.Len() > 0 {
-		cur := heap.Pop(h).(*Item)
-		repeat := min(cur.count, repeatLimit)
-		for range repeat {
-			res = append(res, cur.value)
+	var res []byte
+	for j := 25; j >= 0; {
+		if count[j] == 0 {
+			j--
+			continue
 		}
-
-		if cur.count > repeat && h.Len() > 0 {
-			next := heap.Pop(h).(*Item)
-			res = append(res, next.value)
-			next.count--
-			if next.count > 0 {
-				heap.Push(h, next)
+		for range min(count[j], repeatLimit) {
+			res = append(res, 'a'+byte(j))
+			count[j]--
+		}
+		if count[j] > 0 {
+			i := j - 1
+			for ; i >= 0; i-- {
+				if count[i] != 0 {
+					res = append(res, 'a'+byte(i))
+					count[i]--
+					break
+				}
 			}
-			cur.count -= repeat
-			heap.Push(h, cur)
+			if i < 0 {
+				count[j] = 0
+			}
+		} else {
+			j--
 		}
 	}
-
 	return string(res)
-}
-
-type Heap []*Item
-type Item struct {
-	value byte
-	count int
-}
-
-func (h Heap) Len() int           { return len(h) }
-func (h Heap) Less(i, j int) bool { return h[i].value > h[j].value }
-func (h Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *Heap) Push(x any)        { *h = append(*h, x.(*Item)) }
-func (h *Heap) Pop() any {
-	old := *h
-	n := len(old)
-	res := old[n-1]
-	*h = old[:n-1]
-	return res
 }
