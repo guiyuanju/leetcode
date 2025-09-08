ab5fdab619ec870cefef2a528a8c005d38f8ba21
guiyuanju <guiyuanjune@outlook.com>
2025-09-08 10:45:16 +0800

    upd 2336

--- go/2336/main.go
+++ go/2336/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
-import "container/heap"
+import (
+	"container/heap"
+	"fmt"
+)
 
 func main() {
 	s := Constructor()
@@ -16,53 +18,30 @@ func main() {
 }
 
 type SmallestInfiniteSet struct {
-	heap *Heap
-	cur  int
+	h     Heap
+	top   int
+	exist map[int]bool
 }
 
 func Constructor() SmallestInfiniteSet {
-	return SmallestInfiniteSet{
-		heap: new(Heap),
-		cur:  1,
-	}
+	return SmallestInfiniteSet{nil, 1, map[int]bool{}}
 }
 
 func (this *SmallestInfiniteSet) PopSmallest() int {
-	if this.heap.Len() == 0 {
-		res := this.cur
-		this.cur++
+	if this.h.Len() > 0 {
+		res := heap.Pop(&this.h).(int)
+		this.exist[res] = false
 		return res
 	}
-
-	res := heap.Pop(this.heap).(int)
-	for this.heap.Len() > 0 {
-		cur := heap.Pop(this.heap).(int)
-		if cur != res {
-			heap.Push(this.heap, cur)
-			break
-		}
-	}
-	return res
+	this.top++
+	return this.top - 1
 }
 
 func (this *SmallestInfiniteSet) AddBack(num int) {
-	if num < this.cur {
-		heap.Push(this.heap, num)
+	if num < this.top && !this.exist[num] {
+		heap.Push(&this.h, num)
+		this.exist[num] = true
 	}
-}
-
-type Heap []int
-
-func (h Heap) Len() int           { return len(h) }
-func (h Heap) Less(i, j int) bool { return h[i] < h[j] }
-func (h Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *Heap) Push(x any)        { *h = append(*h, x.(int)) }
-func (h *Heap) Pop() any {
-	old := *h
-	n := len(old)
-	res := old[n-1]
-	*h = old[:n-1]
-	return res
 }
 
 /**
@@ -71,3 +50,15 @@ func (h *Heap) Pop() any {
  * param_1 := obj.PopSmallest();
  * obj.AddBack(num);
  */
+
+type Heap []int
+
+func (h Heap) Len() int           { return len(h) }
+func (h Heap) Less(i, j int) bool { return h[i] < h[j] }
+func (h Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *Heap) Push(x any)        { *h = append(*h, x.(int)) }
+func (h *Heap) Pop() any {
+	hd := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return hd
+}
