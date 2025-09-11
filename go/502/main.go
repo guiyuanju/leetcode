000615f70e9891e066aa12aebc89b8bc20a48960
guiyuanju <guiyuanjune@outlook.com>
2025-09-11 14:35:35 +0800

    upd 502

--- go/502/main.go
+++ go/502/main.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
-import "slices"
-import "container/heap"
+import (
+	"container/heap"
+	"fmt"
+	"slices"
+)
 
 func main() {
 	k := 2
@@ -11,11 +13,7 @@ func main() {
 	capital := []int{0, 1, 1}
 	assertEq(4, findMaximizedCapital(k, w, profits, capital))
 
-	k = 3
-	w = 0
-	profits = []int{1, 2, 3}
-	capital = []int{0, 1, 2}
-	assertEq(6, findMaximizedCapital(k, w, profits, capital))
+	assertEq(6, findMaximizedCapital(3, 0, []int{1, 2, 3}, []int{0, 1, 2}))
 
 	k = 10
 	w = 0
@@ -32,46 +30,42 @@ func assertEq[T comparable](a, b T) {
 	}
 }
 
-type Item struct {
-	profit, capital int
-}
-
 func findMaximizedCapital(k int, w int, profits []int, capital []int) int {
-	var ps []Item
-	for i := range len(profits) {
-		ps = append(ps, Item{profits[i], capital[i]})
+	var maxPros Heap
+	maxPros = Heap{nil, func(i, j int) bool { return profits[maxPros.vals[i]] > profits[maxPros.vals[j]] }}
+
+	indices := make([]int, len(profits))
+	for i := range indices {
+		indices[i] = i
 	}
+	slices.SortFunc(indices, func(i, j int) int { return capital[i] - capital[j] })
 
-	slices.SortFunc(ps, func(a, b Item) int { return a.capital - b.capital })
-
-	h := new(Heap)
+	res := w
 	var i int
-	for k > 0 {
-		for i < len(ps) && ps[i].capital <= w {
-			heap.Push(h, ps[i])
+	for range k {
+		for i < len(indices) && capital[indices[i]] <= res {
+			heap.Push(&maxPros, indices[i])
 			i++
 		}
-		if h.Len() == 0 {
-			break
+		if maxPros.Len() == 0 {
+			return res
 		}
-		top := heap.Pop(h).(Item)
-		w += top.profit
-		k--
+		res += profits[heap.Pop(&maxPros).(int)]
 	}
-
-	return w
+	return res
 }
 
-type Heap []Item
+type Heap struct {
+	vals []int
+	less func(i, j int) bool
+}
 
-func (h Heap) Len() int           { return len(h) }
-func (h Heap) Less(i, j int) bool { return h[i].profit > h[j].profit }
-func (h Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *Heap) Push(x any)        { *h = append(*h, x.(Item)) }
+func (h Heap) Len() int           { return len(h.vals) }
+func (h Heap) Less(i, j int) bool { return h.less(i, j) }
+func (h Heap) Swap(i, j int)      { h.vals[i], h.vals[j] = h.vals[j], h.vals[i] }
+func (h *Heap) Push(x any)        { (*h).vals = append((*h).vals, x.(int)) }
 func (h *Heap) Pop() any {
-	old := *h
-	n := len(old)
-	res := old[n-1]
-	*h = old[:n-1]
-	return res
+	hd := (*h).vals[len((*h).vals)-1]
+	(*h).vals = (*h).vals[:len((*h).vals)-1]
+	return hd
 }
