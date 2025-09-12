831b7d71af3a8f662e232c7a30b1b1c8b977313e
guiyuanju <guiyuanjune@outlook.com>
2025-09-12 15:20:50 +0800

    upd 2384

--- go/2384/main.go
+++ go/2384/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"maps"
 	"slices"
 )
 
@@ -26,38 +25,34 @@ func assertEq[T comparable](a, b T) {
 }
 
 func largestPalindromic(num string) string {
-	count := map[byte]int{}
-	for _, n := range num {
-		count[byte(n)]++
+	count := make([]int, 10)
+	for _, n := range []byte(num) {
+		count[n-'0']++
 	}
-
-	descend := slices.Collect(maps.Keys(count))
-	slices.SortFunc(descend, func(a, b byte) int {
-		return int(b) - int(a)
-	})
 
 	var res []byte
-	for _, b := range descend {
-		if len(res) == 0 && b == '0' {
-			continue
+	for i := 9; i >= 0; i-- {
+		if i == 0 && len(res) == 0 {
+			break
 		}
-		for count[b] >= 2 {
-			res = append(res, b)
-			count[b] -= 2
+		for count[i] >= 2 {
+			res = append(res, byte(i)+'0')
+			count[i] -= 2
 		}
 	}
-	length := len(res)
 
-	for _, b := range descend {
-		if count[b] > 0 {
-			res = append(res, b)
+	right := make([]byte, len(res))
+	copy(right, res)
+	slices.Reverse(right)
+
+	i := 9
+	for ; i >= 0; i-- {
+		if count[i] > 0 {
+			res = append(res, byte(i)+'0')
 			break
 		}
 	}
 
-	for i := range length {
-		res = append(res, res[length-i-1])
-	}
-
+	res = append(res, right...)
 	return string(res)
 }
