42a17c52fff103898cdbdf3f56e14105f317d3ba
guiyuanju <guiyuanjune@outlook.com>
2025-09-12 17:15:37 +0800

    upd 1631

--- go/1631/main.go
+++ go/1631/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"container/heap"
 	"fmt"
-	"math"
 	"strconv"
 	"strings"
 )
@@ -49,48 +47,66 @@ func parse2DArray(s string) [][]int {
 
 // bianry search
 func minimumEffortPath(heights [][]int) int {
+	m := len(heights)
+	n := len(heights[0])
+
+	directions := [][]int{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
 	valid := func(r, c int) bool {
-		return 0 <= r && r < len(heights) && 0 <= c && c < len(heights[0])
+		return 0 <= r && r < m && 0 <= c && c < n
 	}
-	check := func(effort int) bool {
-		seen := map[[2]int]bool{}
-		seen[[2]int{0, 0}] = true
-		var dfs func(r, c int) bool
-		dfs = func(r, c int) bool {
-			if r == len(heights)-1 && c == len(heights[0])-1 {
-				return true
-			}
-			neighbors := [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
-			for _, nei := range neighbors {
-				nextRow, nextCol := r+nei[0], c+nei[1]
-				if valid(nextRow, nextCol) && !seen[[2]int{nextRow, nextCol}] && abs(heights[nextRow][nextCol]-heights[r][c]) <= effort {
-					seen[[2]int{nextRow, nextCol}] = true
-					if dfs(nextRow, nextCol) {
-						return true
-					}
+
+	seen := make([][]bool, m)
+	for i := range m {
+		seen[i] = make([]bool, n)
+	}
+
+	var dfs func(r, c, limit int) bool
+	dfs = func(r, c, limit int) bool {
+		if r == m-1 && c == n-1 {
+			return true
+		}
+		for _, dir := range directions {
+			nr, nc := r+dir[0], c+dir[1]
+			if valid(nr, nc) && !seen[nr][nc] && abs(heights[nr][nc]-heights[r][c]) <= limit {
+				seen[nr][nc] = true
+				if dfs(nr, nc, limit) {
+					return true
 				}
 			}
-			return false
 		}
-		return dfs(0, 0)
+		return false
 	}
 
-	var left, right int
-	for i := range len(heights) {
-		for j := range len(heights[i]) {
-			right = max(right, heights[i][j])
+	// binary search
+	var lo, hi int
+	for i := range m {
+		for j := range n {
+			lo = min(lo, heights[i][j])
+			hi = max(hi, heights[i][j])
 		}
 	}
 
-	for left < right {
-		mid := left + (right-left)/2
-		if check(mid) {
-			right = mid
+	i := 0
+	j := (hi - lo) + 1
+	for i < j {
+		mid := i + (j-i)/2
+		for k := range seen {
+			clear(seen[k])
+		}
+		seen[0][0] = true
+		if dfs(0, 0, mid) {
+			j = mid
 		} else {
-			left = mid + 1
+			i = mid + 1
 		}
 	}
-	return left
+
+	return i
+}
+
+func dbg[T any](x T) T {
+	fmt.Println("dbg:", x)
+	return x
 }
 
 func abs(x int) int {
@@ -98,63 +114,4 @@ func abs(x int) int {
 		return -x
 	}
 	return x
-}
-
-// Dijkstra
-func minimumEffortPath2(heights [][]int) int {
-	neighbors := [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
-	valid := func(r, c int) bool {
-		return 0 <= r && r < len(heights) && 0 <= c && c < len(heights[0])
-	}
-	height := func(r0, c0, r1, c1 int) int {
-		return abs(heights[r0][c0] - heights[r1][c1])
-	}
-	dist := make([][]int, len(heights))
-	for i := range dist {
-		dist[i] = make([]int, len(heights[0]))
-		for j := range dist[i] {
-			dist[i][j] = math.MaxInt
-		}
-	}
-	dist[0][0] = 0
-
-	h := Heap([]Item{{0, 0, 0}})
-	for h.Len() > 0 {
-		cur := heap.Pop(&h).(Item)
-		if cur.dist > dist[cur.r][cur.c] {
-			continue
-		}
-		for _, nei := range neighbors {
-			nextRow, nextCol := cur.r+nei[0], cur.c+nei[1]
-			if !valid(nextRow, nextCol) {
-				continue
-			}
-			neiDist := max(cur.dist, height(nextRow, nextCol, cur.r, cur.c))
-			if neiDist < dist[nextRow][nextCol] {
-				dist[nextRow][nextCol] = neiDist
-				heap.Push(&h, Item{nextRow, nextCol, neiDist})
-			}
-		}
-	}
-
-	return dist[len(heights)-1][len(heights[0])-1]
-}
-
-type Item struct {
-	r, c int
-	dist int
-}
-
-type Heap []Item
-
-func (h Heap) Len() int           { return len(h) }
-func (h Heap) Less(i, j int) bool { return h[i].dist < h[j].dist }
-func (h Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *Heap) Push(x any)        { *h = append(*h, x.(Item)) }
-func (h *Heap) Pop() any {
-	old := *h
-	length := len(old)
-	res := old[length-1]
-	*h = old[:length-1]
-	return res
 }
