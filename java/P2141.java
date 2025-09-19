eb219b5e030c75f785c6c9aa266f3b13ccbfe397
guiyuanju <guiyuanjune@outlook.com>
2025-09-19 12:59:03 +0800

    add 2141

---
+++ java/P2141.java
@@ -0,0 +1,11 @@
+public class P2141 {
+
+    public static void main() {
+        new Solution().maxRunTime(1, new int[] { 1, 2, 3 });
+    }
+}
+
+class Solution {
+
+    public long maxRunTime(int n, int[] batteries) {}
+}
