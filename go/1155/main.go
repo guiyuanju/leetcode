d6283a361fde83e33ba16aaa85ab8f996c98118a
guiyuanju <guiyuanjune@outlook.com>
2025-11-11 10:13:16 +0800

    upd 1155

--- go/1155/main.go
+++ go/1155/main.go
@@ -10,75 +10,62 @@ func main() {
 	fmt.Println(numRollsToTarget(30, 30, 500))
 }
 
-func numRollsToTargetBT(n int, k int, target int) int {
-	var res int
-	var bt func(cur []int, i, remian int)
-	bt = func(cur []int, i, remain int) {
-		if i > n {
-			if remain == 0 {
-				res++
-			}
-			return
-		}
-
-		for v := 1; v <= k && v < remain; v++ {
-			bt(append(cur, v), i+1, remain-v)
-		}
-	}
-
-	bt(nil, 1, target)
-
-	return res
+func numRollsToTarget(n int, k int, target int) int {
+	// return numRollsToTargetTD(n, k, target)
+	return numRollsToTargetBU(n, k, target)
 }
 
 func numRollsToTargetTD(n int, k int, target int) int {
 	memo := map[[2]int]int{}
 
-	var dp func(i, remian int) int
-	dp = func(i, remain int) int {
-		if v, ok := memo[[2]int{i, remain}]; ok {
+	var dp func(i int, target int) int
+	dp = func(i int, target int) int {
+		if v, ok := memo[[2]int{i, target}]; ok {
 			return v
 		}
 
-		if i == 1 {
-			if remain <= k {
+		if i == n {
+			if target == 0 {
 				return 1
 			}
 			return 0
 		}
 
 		var res int
-		for j := 1; j <= k && j < remain; j++ {
-			res += dp(i-1, remain-j)
-			res %= (1e9 + 7)
+		for j := 1; j <= k; j++ {
+			if target-j < 0 {
+				break
+			}
+			res += dp(i+1, target-j)
+			res %= 1e9 + 7
 		}
 
-		memo[[2]int{i, remain}] = res
+		memo[[2]int{i, target}] = res
+
 		return res
 	}
 
-	return dp(n, target)
+	return dp(0, target)
 }
 
-func numRollsToTarget(n int, k int, target int) int {
+func numRollsToTargetBU(n int, k int, target int) int {
 	dp := make([][]int, n+1)
 	for i := range dp {
 		dp[i] = make([]int, target+1)
 	}
-	for i := range target + 1 {
-		if i <= k {
-			dp[1][i] = 1
-		}
-	}
+	dp[n][0] = 1
 
-	for i := 2; i <= n; i++ {
-		for j := 0; j <= target; j++ {
-			for v := 1; v <= k && v < j; v++ {
-				dp[i][j] += dp[i-1][j-v]
-				dp[i][j] %= 1e9 + 7
+	for i := n - 1; i >= 0; i-- {
+		for remain := 1; remain <= target; remain++ {
+			for j := 1; j <= k; j++ {
+				if remain-j < 0 {
+					break
+				}
+				dp[i][remain] += dp[i+1][remain-j]
+				dp[i][remain] %= 1e9 + 7
 			}
 		}
 	}
 
-	return dp[n][target]
+	return dp[0][target]
 }
