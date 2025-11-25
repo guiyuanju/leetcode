ad7bb2c583ecb1eaae767bf5d657f7bf989bffe0
guiyuanju <guiyuanjune@outlook.com>
2025-11-26 07:45:19 +0800

    upd 1094

--- go/1094/main.go
+++ go/1094/main.go
@@ -1,85 +1,27 @@
 package main
 
 import (
-	"math"
-	"slices"
-
 	"github.com/guiyuanju/lcutils/assert"
 )
 
 func main() {
-	assert.Eq(false, carPooling3([][]int{{2, 1, 5}, {3, 3, 7}}, 4))
-	assert.Eq(true, carPooling3([][]int{{2, 1, 5}, {3, 3, 7}}, 5))
-	assert.Eq(true, carPooling3([][]int{{3, 2, 9}, {3, 2, 8}, {5, 2, 5}, {7, 4, 8}, {8, 7, 8}, {9, 1, 2}}, 22))
+	assert.Eq(false, carPooling([][]int{{2, 1, 5}, {3, 3, 7}}, 4))
+	assert.Eq(true, carPooling([][]int{{2, 1, 5}, {3, 3, 7}}, 5))
+	assert.Eq(true, carPooling([][]int{{3, 2, 9}, {3, 2, 8}, {5, 2, 5}, {7, 4, 8}, {8, 7, 8}, {9, 1, 2}}, 22))
 }
 
-// O(n*log(n)), better than solution 2
-func carPooling3(trips [][]int, capacity int) bool {
-	diff := make([][]int, 0, len(trips)*2)
-	for _, t := range trips {
-		diff = append(diff, []int{t[1], t[0]})
-		diff = append(diff, []int{t[2], -t[0]})
-	}
-
-	slices.SortFunc(diff, func(a, b []int) int { return a[0] - b[0] })
-
-	var cur int
-	for _, d := range diff {
-		cur += d[1]
-		if cur > capacity {
-			return false
-		}
-	}
-
-	return true
-}
-
-// O(n*log(n))
-func carPooling2(trips [][]int, capacity int) bool {
-	starts := slices.SortedFunc(slices.Values(trips), func(a, b []int) int { return a[1] - b[1] })
-	ends := slices.SortedFunc(slices.Values(trips), func(a, b []int) int { return a[2] - b[2] })
-
-	var i, j, cur int
-	for i < len(trips) && j < len(trips) {
-		if starts[i][1] < ends[j][2] {
-			cur += starts[i][0]
-			i++
-		} else if starts[i][1] > ends[j][2] {
-			cur -= ends[j][0]
-			j++
-		} else {
-			cur += starts[i][0]
-			cur -= ends[j][0]
-			i++
-			j++
-		}
-
-		if cur > capacity {
-			return false
-		}
-	}
-
-	return true
-}
-
-// difference array, better time & space, O(m + n)
 func carPooling(trips [][]int, capacity int) bool {
-	smallest := math.MaxInt
-	largest := math.MinInt
-	for _, t := range trips {
-		smallest = min(smallest, t[2])
-		largest = max(largest, t[2])
-	}
-
-	diff := make([]int, largest+1)
+	diff := make([]int, 1001)
+	hi := 0
 	for _, t := range trips {
 		diff[t[1]] += t[0]
 		diff[t[2]] -= t[0]
+		hi = max(hi, t[1], t[2])
 	}
 
 	var cur int
-	for _, d := range diff {
-		cur += d
+	for i := range hi + 1 {
+		cur += diff[i]
 		if cur > capacity {
 			return false
 		}
