db4bb9f92b1fa0f0755132ed9659da051557979d
guiyuanju <guiyuanjune@outlook.com>
2025-12-05 09:24:41 +0800

    upd 743

--- go/743/main.go
+++ go/743/main.go
@@ -4,56 +4,74 @@ import (
 	"container/heap"
 	"fmt"
 	"math"
-	"slices"
 )
 
 func main() {
-	fmt.Println(networkDelayTime([][]int{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2))
-	fmt.Println(networkDelayTime([][]int{{1, 2, 1}}, 2, 1))
-	fmt.Println(networkDelayTime([][]int{{1, 2, 1}}, 2, 2))
+	assertEq(2, networkDelayTime([][]int{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}}, 4, 2))
+	assertEq(1, networkDelayTime([][]int{{1, 2, 1}}, 2, 1))
+	assertEq(-1, networkDelayTime([][]int{{1, 2, 1}}, 2, 2))
+}
+
+func assertEq[T comparable](a, b T) {
+	if a == b {
+		fmt.Printf("✓ %v == %v\n", a, b)
+	} else {
+		fmt.Printf("✗ %v != %v\n", a, b)
+	}
 }
 
 func networkDelayTime(times [][]int, n int, k int) int {
-	g := map[int][][]int{}
-	for _, e := range times {
-		g[e[0]] = append(g[e[0]], []int{e[1], e[2]})
+	graph := make(map[int][][2]int, n)
+	for _, t := range times {
+		graph[t[0]] = append(graph[t[0]], [2]int{t[1], t[2]})
 	}
 
 	dist := make([]int, n+1)
+
 	for i := range dist {
 		dist[i] = math.MaxInt
 	}
 
-	h := Heap([][2]int{{k, 0}})
+	h := Heap{
+		nodes: []int{k},
+		less:  func(a, b int) bool { return dist[a] < dist[b] },
+	}
+
+	dist[k] = 0
+
 	for h.Len() > 0 {
-		cur := heap.Pop(&h).([2]int)
-		if dist[cur[0]] <= cur[1] {
-			continue
-		}
-		dist[cur[0]] = cur[1]
-		for _, nei := range g[cur[0]] {
-			// important optimization
-			if nei[1]+cur[1] < dist[nei[0]] {
-				heap.Push(&h, [2]int{nei[0], nei[1] + cur[1]})
+		cur := heap.Pop(&h).(int)
+		for _, nei := range graph[cur] {
+			if dist[cur]+nei[1] < dist[nei[0]] {
+				dist[nei[0]] = dist[cur] + nei[1]
+				heap.Push(&h, nei[0])
 			}
 		}
 	}
 
-	res := slices.Max(dist[1:])
-	if res == math.MaxInt {
-		return -1
+	var res int
+	for i := 1; i < len(dist); i++ {
+		d := dist[i]
+		if d == math.MaxInt {
+			return -1
+		}
+		res = max(res, d)
 	}
 	return res
 }
 
-type Heap [][2]int
+type Heap struct {
+	nodes []int
+	less  func(int, int) bool
+}
 
-func (h Heap) Len() int           { return len(h) }
-func (h Heap) Less(i, j int) bool { return h[i][1] < h[j][1] }
-func (h Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *Heap) Push(x any)        { *h = append(*h, x.([2]int)) }
+func (h Heap) Len() int           { return len(h.nodes) }
+func (h Heap) Less(a, b int) bool { return h.less(h.nodes[a], h.nodes[b]) }
+func (h *Heap) Swap(a, b int)     { h.nodes[a], h.nodes[b] = h.nodes[b], h.nodes[a] }
+func (h *Heap) Push(x any)        { h.nodes = append(h.nodes, x.(int)) }
 func (h *Heap) Pop() any {
-	hd := (*h)[len(*h)-1]
-	*h = (*h)[:len(*h)-1]
-	return hd
+	oldLen := len(h.nodes)
+	res := h.nodes[oldLen-1]
+	h.nodes = h.nodes[:oldLen-1]
+	return res
 }
