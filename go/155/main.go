bebc27c5152237927d353738b72921282a5ad0e5
guiyuanju <guiyuanjune@outlook.com>
2025-12-12 10:30:35 +0800

    upd 155

--- go/155/main.go
+++ go/155/main.go
@@ -1,11 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+)
+
 func main() {
+	ms := Constructor()
+	ms.Push(-2)
+	ms.Push(0)
+	ms.Push(-3)
+	assertEq(-3, ms.GetMin())
+	ms.Pop()
+	assertEq(0, ms.Top())
+	assertEq(-2, ms.GetMin())
+}
+
+func assertEq(a, b any) {
+	if reflect.DeepEqual(a, b) {
+		fmt.Printf("Ok: %v\n", a)
+	} else {
+		fmt.Printf("Failed: %v != %v\n", a, b)
+	}
 }
 
 type MinStack struct {
-	stack []int
-	mins  []int
+	eles []int
+	mins []int
 }
 
 func Constructor() MinStack {
@@ -13,32 +34,22 @@ func Constructor() MinStack {
 }
 
 func (this *MinStack) Push(val int) {
-	this.stack = append(this.stack, val)
-	if len(this.mins) == 0 {
-		this.mins = append(this.mins, val)
-	} else {
-		this.mins = append(this.mins, min(val, this.mins[len(this.mins)-1]))
+	this.eles = append(this.eles, val)
+	if len(this.mins) > 0 {
+		val = min(val, this.mins[len(this.mins)-1])
 	}
+	this.mins = append(this.mins, val)
 }
 
 func (this *MinStack) Pop() {
-	this.stack = this.stack[:len(this.stack)-1]
+	this.eles = this.eles[:len(this.eles)-1]
 	this.mins = this.mins[:len(this.mins)-1]
 }
 
 func (this *MinStack) Top() int {
-	return this.stack[len(this.stack)-1]
+	return this.eles[len(this.eles)-1]
 }
 
 func (this *MinStack) GetMin() int {
 	return this.mins[len(this.mins)-1]
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Push(val);
- * obj.Pop();
- * param_3 := obj.Top();
- * param_4 := obj.GetMin();
- */
