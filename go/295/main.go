acdae4c2efd64b025edfc8c9a009ed9c1b7e77b7
guiyuanju <guiyuanjune@outlook.com>
2025-12-19 18:01:35 +0800

    upd 295

--- go/295/main.go
+++ go/295/main.go
@@ -24,6 +24,13 @@ func main() {
 	assertEq(-2.5, mf.FindMedian())
 	mf.AddNum(-5)
 	assertEq(-3.0, mf.FindMedian())
+
+	mf = Constructor()
+	mf.AddNum(1)
+	mf.AddNum(2)
+	assertEq(1.5, mf.FindMedian())
+	mf.AddNum(3)
+	assertEq(2, mf.FindMedian())
 }
 
 func assertEq[T comparable](a, b T) {
@@ -34,20 +41,8 @@ func assertEq[T comparable](a, b T) {
 	}
 }
 
-type Heap []int
-
-func (h Heap) Len() int           { return len(h) }
-func (h Heap) Less(i, j int) bool { return h[i] < h[j] }
-func (h Heap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *Heap) Push(x any)        { *h = append(*h, x.(int)) }
-func (h *Heap) Pop() any {
-	hd := (*h)[len(*h)-1]
-	*h = (*h)[:len(*h)-1]
-	return hd
-}
-
 type MedianFinder struct {
-	left, right Heap
+	lo, hi Heap
 }
 
 func Constructor() MedianFinder {
@@ -55,23 +50,31 @@ func Constructor() MedianFinder {
 }
 
 func (this *MedianFinder) AddNum(num int) {
-	heap.Push(&(this.right), num)
-	heap.Push(&(this.left), -heap.Pop(&(this.right)).(int))
-	if this.left.Len()-this.right.Len() == 2 {
-		heap.Push(&(this.right), -heap.Pop(&(this.left)).(int))
+	heap.Push(&this.lo, -num)
+	if len(this.lo) > len(this.hi) {
+		heap.Push(&this.hi, -heap.Pop(&this.lo).(int))
+	}
+	if len(this.lo) < len(this.hi) {
+		heap.Push(&this.lo, -heap.Pop(&this.hi).(int))
 	}
 }
 
 func (this *MedianFinder) FindMedian() float64 {
-	if this.left.Len() > this.right.Len() {
-		return -float64(this.left[0])
+	if len(this.lo) > len(this.hi) {
+		return float64(-this.lo[0])
 	}
-	return (-float64(this.left[0]) + float64(this.right[0])) / 2
+	return float64(-this.lo[0]+this.hi[0]) / 2
 }
 
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * obj := Constructor();
- * obj.AddNum(num);
- * param_2 := obj.FindMedian();
- */
+type Heap []int
+
+func (h Heap) Len() int           { return len(h) }
+func (h Heap) Less(i, j int) bool { return h[i] < h[j] }
+func (h *Heap) Swap(i, j int)     { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+func (h *Heap) Push(x any)        { *h = append(*h, x.(int)) }
+func (h *Heap) Pop() any {
+	l := len(*h) - 1
+	res := (*h)[l]
+	*h = (*h)[:l]
+	return res
+}
