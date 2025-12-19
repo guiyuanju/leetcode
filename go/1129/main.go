da82a776b6f7190cfbbdc946be467754c6b1c3aa
guiyuanju <guiyuanjune@outlook.com>
2025-12-19 14:44:02 +0800

    upd 1129

--- go/1129/main.go
+++ go/1129/main.go
@@ -1,57 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println(shortestAlternatingPaths(3, [][]int{{0, 1}, {1, 2}}, [][]int{}))
-	fmt.Println(shortestAlternatingPaths(3, [][]int{{0, 1}}, [][]int{{2, 1}}))
+	assertEq([]int{0, 1, -1}, shortestAlternatingPaths(3, [][]int{{0, 1}, {1, 2}}, [][]int{}))
+	assertEq([]int{0, 1, -1}, shortestAlternatingPaths(3, [][]int{{0, 1}}, [][]int{{2, 1}}))
+	assertEq([]int{0, 1, 2}, shortestAlternatingPaths(3, [][]int{{0, 1}}, [][]int{{1, 2}}))
+}
+
+func assertEq(a, b any) {
+	if reflect.DeepEqual(a, b) {
+		fmt.Printf("Ok: %v\n", a)
+	} else {
+		fmt.Printf("Failed: %v != %v\n", a, b)
+	}
 }
 
 func shortestAlternatingPaths(n int, redEdges [][]int, blueEdges [][]int) []int {
-	const (
-		RED  = 0
-		BLUE = 1
-	)
-
-	g := [2][][]int{make([][]int, n), make([][]int, n)}
+	g := [2]map[int][]int{{}, {}}
 	for _, e := range redEdges {
-		g[RED][e[0]] = append(g[RED][e[0]], e[1])
+		g[0][e[0]] = append(g[0][e[0]], e[1])
 	}
 	for _, e := range blueEdges {
-		g[BLUE][e[0]] = append(g[BLUE][e[0]], e[1])
+		g[1][e[0]] = append(g[1][e[0]], e[1])
 	}
 
 	type node struct {
-		n, color int // 0 - red, 1 - blue
+		n, c, s int
 	}
+
+	seen := map[[2]int]bool{}
+	seen[[2]int{0, 0}] = true
+	seen[[2]int{0, 1}] = true
+	q := []node{{0, 0, 0}, {0, 1, 0}}
 
 	res := make([]int, n)
-	for i := range n {
+	for i := range res {
 		res[i] = -1
 	}
-	res[0] = 0
 
-	seen := map[node]bool{} // key 1: node as state
-	seen[node{0, 0}] = true
-	seen[node{0, 1}] = true
-	queue := []node{{0, 0}, {0, 1}} // key 2: red and blue node 0
-
-	step := 1
-	for len(queue) > 0 {
-		for range len(queue) {
-			cur := queue[0]
-			queue = queue[1:]
-			for _, nei := range g[1-cur.color][cur.n] {
-				if !seen[node{nei, 1 - cur.color}] {
-					seen[node{nei, 1 - cur.color}] = true
-					queue = append(queue, node{nei, 1 - cur.color})
-					if res[nei] == -1 { // key 3: keep the smallest (BFS the first is the smallest)
-						res[nei] = step
-					}
-				}
+	for len(q) > 0 {
+		cur := q[0]
+		q = q[1:]
+		if res[cur.n] == -1 {
+			res[cur.n] = cur.s
+		}
+		for _, nei := range g[1-cur.c][cur.n] {
+			if !seen[[2]int{nei, 1 - cur.c}] {
+				seen[[2]int{nei, 1 - cur.c}] = true
+				q = append(q, node{nei, 1 - cur.c, cur.s + 1})
 			}
 		}
-		step++
 	}
 
 	return res
