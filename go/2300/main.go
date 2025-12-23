d9131fc526841b8b0828fe2d72f0edb559584d29
guiyuanju <guiyuanjune@outlook.com>
2025-12-23 10:15:05 +0800

    upd 2300

--- go/2300/main.go
+++ go/2300/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"reflect"
 	"slices"
 )
 
@@ -9,33 +11,42 @@ func main() {
 	spells := []int{5, 1, 3}
 	potions := []int{1, 2, 3, 4, 5}
 	var success int64 = 7
-	fmt.Println(successfulPairs(spells, potions, int64(success)))
+	assertEq([]int{4, 0, 3}, successfulPairs(spells, potions, int64(success)))
 
 	spells = []int{3, 1, 2}
 	potions = []int{8, 5, 8}
 	success = 16
-	fmt.Println(successfulPairs(spells, potions, int64(success)))
+	assertEq([]int{2, 0, 2}, successfulPairs(spells, potions, int64(success)))
+}
+
+func assertEq(a, b any) {
+	if reflect.DeepEqual(a, b) {
+		fmt.Printf("Ok: %v\n", a)
+	} else {
+		fmt.Printf("Failed: %v != %v\n", a, b)
+	}
 }
 
 func successfulPairs(spells []int, potions []int, success int64) []int {
+	var res []int
 	slices.Sort(potions)
-	res := make([]int, len(spells))
-	for i := range res {
-		res[i] = len(potions) - bs(potions, float64(success)/float64(spells[i]))
+	for _, s := range spells {
+		target := int64(math.Ceil(float64(success) / float64(s)))
+		res = append(res, len(potions)-bs(potions, target))
 	}
 	return res
 }
 
-func bs(arr []int, target float64) int {
-	i := 0
-	j := len(arr)
-	for i < j {
-		mid := i + (j-i)/2
-		if float64(arr[mid]) >= target {
-			j = mid
+func bs(xs []int, target int64) int {
+	lo := 0
+	hi := len(xs)
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if int64(xs[mid]) < target {
+			lo = mid + 1
 		} else {
-			i = mid + 1
+			hi = mid
 		}
 	}
-	return i
+	return lo
 }
