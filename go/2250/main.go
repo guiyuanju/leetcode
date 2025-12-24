e692b0658334cec890fe0b07598744671b6a68e2
guiyuanju <guiyuanjune@outlook.com>
2025-12-24 14:53:07 +0800

    upd 2250

--- go/2250/main.go
+++ go/2250/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"slices"
 	"strconv"
 	"strings"
@@ -10,15 +11,23 @@ import (
 func main() {
 	rect := parse2DArray("[[1,2],[2,3],[2,5]]")
 	points := parse2DArray("[[2,1],[1,4]]")
-	fmt.Println(countRectangles(rect, points))
+	assertEq([]int{2, 1}, countRectangles(rect, points))
 
 	rect = parse2DArray("[[1,1],[2,2],[3,3]]")
 	points = parse2DArray("[[1,3],[1,1]]")
-	fmt.Println(countRectangles(rect, points))
+	assertEq([]int{1, 3}, countRectangles(rect, points))
 
 	rect = parse2DArray("[[4,7],[4,9],[8,5],[6,2],[6,4]]")
 	points = parse2DArray("[[4,2],[5,6]]")
-	fmt.Println(countRectangles(rect, points))
+	assertEq([]int{5, 0}, countRectangles(rect, points))
+}
+
+func assertEq(a, b any) {
+	if reflect.DeepEqual(a, b) {
+		fmt.Printf("Ok: %v\n", a)
+	} else {
+		fmt.Printf("Failed: %v != %v\n", a, b)
+	}
 }
 
 func parse2DArray(s string) [][]int {
@@ -46,32 +55,29 @@ func countRectangles(rectangles [][]int, points [][]int) []int {
 	for _, r := range rectangles {
 		widths[r[1]] = append(widths[r[1]], r[0])
 	}
-	for _, ws := range widths {
-		slices.SortFunc(ws, func(a, b int) int { return b - a })
+	for i := range widths {
+		slices.SortFunc(widths[i], func(a, b int) int { return b - a })
 	}
 
-	bs := func(p []int, nums []int) int {
-		left := 0
-		right := len(nums)
-		for left < right {
-			mid := left + (right-left)/2
-			if nums[mid] < p[0] {
-				right = mid
+	bs := func(xs []int, target int) int {
+		i := 0
+		j := len(xs)
+		for i < j {
+			mid := i + (j-i)/2
+			if xs[mid] >= target {
+				i = mid + 1
 			} else {
-				left = mid + 1
+				j = mid
 			}
 		}
-		return left
+		return i
 	}
 
-	var res []int
-	for _, p := range points {
-		var cur int
-		for i := p[1]; i <= 100; i++ {
-			cur += bs(p, widths[i])
+	res := make([]int, len(points))
+	for i, p := range points {
+		for j := p[1]; j < len(widths); j++ {
+			res[i] += bs(widths[j], p[0])
 		}
-		res = append(res, cur)
 	}
-
 	return res
 }
