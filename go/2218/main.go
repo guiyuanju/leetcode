30bccd31327be843256cfc739950703bf6e9abe7
guiyuanju <guiyuanjune@outlook.com>
2025-12-27 11:41:36 +0800

    upd 2218

--- go/2218/main.go
+++ go/2218/main.go
@@ -1,40 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println(maxValueOfCoins([][]int{{1, 100, 3}, {7, 8, 9}}, 2))
-	fmt.Println(maxValueOfCoins([][]int{{100}, {100}, {100}, {100}, {100}, {100}, {1, 1, 1, 1, 1, 1, 700}}, 7))
+	assertEq(101, maxValueOfCoins([][]int{{1, 100, 3}, {7, 8, 9}}, 2))
+	assertEq(706, maxValueOfCoins([][]int{{100}, {100}, {100}, {100}, {100}, {100}, {1, 1, 1, 1, 1, 1, 700}}, 7))
+	assertEq(494, maxValueOfCoins([][]int{{37, 88}, {51, 64, 65, 20, 95, 30, 26}, {9, 62, 20}, {44}}, 9))
+}
+
+func assertEq(a, b any) {
+	if reflect.DeepEqual(a, b) {
+		fmt.Printf("Ok: %v\n", a)
+	} else {
+		fmt.Printf("Failed: %v != %v\n", a, b)
+	}
 }
 
 func maxValueOfCoins(piles [][]int, k int) int {
-	// return maxValueOfCoinsTD(piles, k)
-	return maxValueOfCoinsBU(piles, k)
+	// return maxValueOfCoins_td(piles, k)
+	return maxValueOfCoins_bu(piles, k)
 }
 
-// top down
-func maxValueOfCoinsTD(piles [][]int, k int) int {
-	type key struct{ i, left int }
-	memo := map[key]int{}
-
-	var dp func(i, left int) int
-	dp = func(i, left int) int {
-		if v, ok := memo[key{i, left}]; ok {
-			return v
-		}
-
-		if i == len(piles) || left == 0 {
+func maxValueOfCoins_td(piles [][]int, k int) int {
+	var dp func(i int, k int) int
+	dp = func(i int, k int) int {
+		if i >= len(piles) || k == 0 {
 			return 0
 		}
 
 		var cur int
-		res := dp(i+1, left)
-		for j := range min(left, len(piles[i])) {
+		res := dp(i+1, k)
+		for j := range min(k, len(piles[i])) {
 			cur += piles[i][j]
-			res = max(res, cur+dp(i+1, left-j-1))
+			res = max(res, cur+dp(i+1, k-j-1))
 		}
-
-		memo[key{i, left}] = res
 
 		return res
 	}
@@ -42,7 +44,7 @@ func maxValueOfCoinsTD(piles [][]int, k int) int {
 	return dp(0, k)
 }
 
-func maxValueOfCoinsBU(piles [][]int, k int) int {
+func maxValueOfCoins_bu(piles [][]int, k int) int {
 	dp := make([][]int, len(piles)+1)
 	for i := range dp {
 		dp[i] = make([]int, k+1)
@@ -51,12 +53,11 @@ func maxValueOfCoinsBU(piles [][]int, k int) int {
 	for i := len(piles) - 1; i >= 0; i-- {
 		for j := 1; j <= k; j++ {
 			var cur int
-			res := dp[i+1][j]
+			dp[i][j] = dp[i+1][j]
 			for k := range min(j, len(piles[i])) {
 				cur += piles[i][k]
-				res = max(res, cur+dp[i+1][j-k-1])
+				dp[i][j] = max(dp[i][j], cur+dp[i+1][j-k-1])
 			}
-			dp[i][j] = res
 		}
 	}
 
