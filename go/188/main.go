742e16368b9e145d85d630f6305112155b2bfb33
guiyuanju <guiyuanjune@outlook.com>
2025-12-27 10:48:46 +0800

    upd 188

--- go/188/main.go
+++ go/188/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func main() {
-	fmt.Println(maxProfit(2, []int{2, 4, 1}))
-	fmt.Println(maxProfit(2, []int{3, 2, 6, 5, 0, 3}))
+	assertEq(2, maxProfit(2, []int{2, 4, 1}))
+	assertEq(7, maxProfit(2, []int{3, 2, 6, 5, 0, 3}))
+}
+
+func assertEq(a, b any) {
+	if reflect.DeepEqual(a, b) {
+		fmt.Printf("Ok: %v\n", a)
+	} else {
+		fmt.Printf("Failed: %v != %v\n", a, b)
+	}
 }
 
 func maxProfit(k int, prices []int) int {
@@ -15,54 +24,40 @@ func maxProfit(k int, prices []int) int {
 }
 
 func maxProfit_td(k int, prices []int) int {
-	type key struct {
-		i, remain int
-		holding   bool
-	}
-	memo := map[key]int{}
-	var dp func(i, remain int, holding bool) int
-	dp = func(i, remain int, holding bool) int {
-		if v, ok := memo[key{i, remain, holding}]; ok {
-			return v
-		}
-
-		if i == len(prices) {
+	var dp func(i int, k int, holding int) int
+	dp = func(i int, k int, holding int) int {
+		if i >= len(prices) {
 			return 0
 		}
 
-		res := dp(i+1, remain, holding)
-		if holding {
-			res = max(prices[i]+dp(i+1, remain, false), res)
-		} else if remain > 0 {
-			res = max(-prices[i]+dp(i+1, remain-1, true), res)
+		res := dp(i+1, k, holding)
+		if holding == 1 {
+			res = max(res, prices[i]+dp(i+1, k, 0))
+		} else if k > 0 {
+			res = max(res, -prices[i]+dp(i+1, k-1, 1))
 		}
-
-		memo[key{i, remain, holding}] = res
 
 		return res
 	}
 
-	return dp(0, k, false)
+	return dp(0, k, 0)
 }
 
 func maxProfit_bu(k int, prices []int) int {
 	dp := make([][][]int, len(prices)+1)
 	for i := range dp {
 		dp[i] = make([][]int, k+1)
-		for j := range len(dp[i]) {
+		for j := range dp[i] {
 			dp[i][j] = make([]int, 2)
 		}
 	}
 
 	for i := len(prices) - 1; i >= 0; i-- {
-		for j := range k + 1 {
-			for k := range 2 {
-				dp[i][j][k] = dp[i+1][j][k]
-				if k == 1 {
-					dp[i][j][k] = max(prices[i]+dp[i+1][j][0], dp[i][j][k])
-				} else if j > 0 {
-					dp[i][j][k] = max(-prices[i]+dp[i+1][j-1][1], dp[i][j][k])
-				}
+		for curK := 0; curK <= k; curK++ {
+			copy(dp[i][curK], dp[i+1][curK])
+			dp[i][curK][1] = max(dp[i][curK][1], prices[i]+dp[i+1][curK][0])
+			if curK > 0 {
+				dp[i][curK][0] = max(dp[i][curK][0], -prices[i]+dp[i+1][curK-1][1])
 			}
 		}
 	}
