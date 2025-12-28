390c816d61c58142fa647c67055cf1e20f011b8d
guiyuanju <guiyuanjune@outlook.com>
2025-12-28 09:48:25 +0800

    upd 1035

--- go/1035/main.go
+++ go/1035/main.go
@@ -11,38 +11,28 @@ func main() {
 }
 
 func maxUncrossedLines(nums1 []int, nums2 []int) int {
-	return maxUncrossedLinesBU(nums1, nums2)
-	// return maxUncrossedLinesUD(nums1, nums2)
+	return maxUncrossedLines_td(nums1, nums2)
+	// return maxUncrossedLines_bu(nums1, nums2)
 }
 
-func maxUncrossedLinesUD(nums1 []int, nums2 []int) int {
-	memo := map[[2]int]int{}
+func maxUncrossedLines_td(nums1 []int, nums2 []int) int {
 	var dp func(i, j int) int
 	dp = func(i, j int) int {
-		if v, ok := memo[[2]int{i, j}]; ok {
-			return v
-		}
-
 		if i >= len(nums1) || j >= len(nums2) {
 			return 0
 		}
 
-		var res int
 		if nums1[i] == nums2[j] {
-			res = 1 + dp(i+1, j+1)
-		} else {
-			res = max(dp(i+1, j), dp(i, j+1))
+			return dp(i+1, j+1) + 1
 		}
 
-		memo[[2]int{i, j}] = res
-
-		return res
+		return max(dp(i+1, j), dp(i, j+1))
 	}
 
 	return dp(0, 0)
 }
 
-func maxUncrossedLinesBU(nums1 []int, nums2 []int) int {
+func maxUncrossedLines_bu(nums1 []int, nums2 []int) int {
 	dp := make([][]int, len(nums1)+1)
 	for i := range dp {
 		dp[i] = make([]int, len(nums2)+1)
@@ -53,7 +43,7 @@ func maxUncrossedLinesBU(nums1 []int, nums2 []int) int {
 			if nums1[i] == nums2[j] {
 				dp[i][j] = 1 + dp[i+1][j+1]
 			} else {
-				dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+				dp[i][j] = max(dp[i][j], dp[i+1][j], dp[i][j+1])
 			}
 		}
 	}
