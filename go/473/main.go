bc0b7f9cd7bd39cc775341fbfd71f6e5557d6a6c
guiyuanju <guiyuanjune@outlook.com>
2026-01-04 13:53:17 +0800

    upd 473

--- go/473/main.go
+++ go/473/main.go
@@ -2,93 +2,111 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"slices"
 )
 
 func main() {
-	fmt.Println(makesquare([]int{1, 1, 2, 2, 2}))
-	fmt.Println(makesquare([]int{3, 3, 3, 3, 4}))
-	fmt.Println(makesquare([]int{5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3}))
+	assertEq(true, makesquare([]int{1, 1, 2, 2, 2}))
+	assertEq(false, makesquare([]int{3, 3, 3, 3, 4}))
+	assertEq(true, makesquare([]int{5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3}))
 }
 
-// backtracking
-// time limit exceeded
-func makesquareBt(matchsticks []int) bool {
-	var sum int
-	for _, n := range matchsticks {
-		sum += n
+func assertEq(a, b any) {
+	if reflect.DeepEqual(a, b) {
+		fmt.Printf("Ok: %v\n", a)
+	} else {
+		fmt.Printf("Failed: %v != %v\n", a, b)
 	}
-	if sum%4 != 0 {
-		return false
-	}
-	length := sum / 4
-	slices.Sort(matchsticks)
-	edges := [4]int{}
-
-	var bt func(i int) bool
-	bt = func(i int) bool {
-		if i == len(matchsticks) {
-			return true
-		}
-
-		for j := range 4 {
-			if edges[j]+matchsticks[i] > length {
-				continue
-			}
-			edges[j] += matchsticks[i]
-			if bt(i + 1) {
-				return true
-			}
-			edges[j] -= matchsticks[i]
-		}
-		return false
-	}
-
-	return bt(0)
 }
 
 func makesquare(matchsticks []int) bool {
 	var sum int
-	for _, n := range matchsticks {
-		sum += n
+	for _, l := range matchsticks {
+		sum += l
 	}
 	if sum%4 != 0 {
 		return false
 	}
-	length := sum / 4
+	width := sum / 4
 
-	type key struct {
+	type state struct {
 		i    int
 		conf [4]int
 	}
-
-	memo := map[key]bool{}
+	memo := map[state]bool{}
 
 	var dp func(i int, conf [4]int) bool
 	dp = func(i int, conf [4]int) bool {
 		slices.Sort(conf[:])
-		if v, ok := memo[key{i, conf}]; ok {
+		if v, ok := memo[state{i, conf}]; ok {
 			return v
 		}
 
-		if i == len(matchsticks) {
+		if i >= len(matchsticks) {
 			return true
 		}
 
-		for j := range conf {
-			if conf[j]+matchsticks[i] <= length {
-				conf[j] += matchsticks[i]
+		for j, c := range conf {
+			if c >= matchsticks[i] {
+				conf[j] -= matchsticks[i]
 				if dp(i+1, conf) {
-					memo[key{i, conf}] = true
+					memo[state{i, conf}] = true
 					return true
 				}
-				conf[j] -= matchsticks[i]
+				conf[j] += matchsticks[i]
 			}
 		}
 
-		memo[key{i, conf}] = false
+		memo[state{i, conf}] = false
 		return false
 	}
 
-	return dp(0, [4]int{})
+	return dp(0, [4]int{width, width, width, width})
 }
+
+// func makesquare(matchsticks []int) bool {
+// 	var sum int
+// 	for _, n := range matchsticks {
+// 		sum += n
+// 	}
+// 	if sum%4 != 0 {
+// 		return false
+// 	}
+// 	length := sum / 4
+//
+// 	type key struct {
+// 		i    int
+// 		conf [4]int
+// 	}
+//
+// 	memo := map[key]bool{}
+//
+// 	var dp func(i int, conf [4]int) bool
+// 	dp = func(i int, conf [4]int) bool {
+// 		slices.Sort(conf[:])
+// 		if v, ok := memo[key{i, conf}]; ok {
+// 			return v
+// 		}
+//
+// 		if i == len(matchsticks) {
+// 			return true
+// 		}
+//
+// 		for j := range conf {
+// 			if conf[j]+matchsticks[i] <= length {
+// 				conf[j] += matchsticks[i]
+// 				if dp(i+1, conf) {
+// 					memo[key{i, conf}] = true
+// 					return true
+// 				}
+// 				conf[j] -= matchsticks[i]
+// 			}
+// 		}
+//
+// 		memo[key{i, conf}] = false
+// 		return false
+// 	}
+//
+// 	return dp(0, [4]int{})
+// }
