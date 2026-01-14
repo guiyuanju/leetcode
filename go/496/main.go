a492d08ef9666088f7153c025c8b0a0076cb6842
guiyuanju <guiyuanjune@outlook.com>
2026-01-14 13:40:22 +0800

    upd 496

--- go/496/main.go
+++ go/496/main.go
@@ -1,31 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println(nextGreaterElement([]int{4, 1, 2}, []int{1, 3, 4, 2}))
-	fmt.Println(nextGreaterElement([]int{2, 4}, []int{1, 2, 3, 4}))
+	assertEq([]int{-1, 3, -1}, nextGreaterElement([]int{4, 1, 2}, []int{1, 3, 4, 2}))
+	assertEq([]int{3, -1}, nextGreaterElement([]int{2, 4}, []int{1, 2, 3, 4}))
+}
+
+func assertEq(a, b any) {
+	if reflect.DeepEqual(a, b) {
+		fmt.Printf("Ok: %v\n", a)
+	} else {
+		fmt.Printf("Failed: %v != %v\n", a, b)
+	}
 }
 
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
 	m := map[int]int{}
-	s := []int{}
+	mono := []int{}
 	for _, n := range nums2 {
-		for len(s) > 0 && s[len(s)-1] < n {
-			popped := s[len(s)-1]
-			s = s[:len(s)-1]
-			m[popped] = n
+		for len(mono) > 0 && n > mono[len(mono)-1] {
+			m[mono[len(mono)-1]] = n
+			mono = mono[:len(mono)-1]
 		}
-		s = append(s, n)
+		mono = append(mono, n)
 	}
 
-	var res []int
-	for _, n := range nums1 {
-		ng := -1
-		if v, ok := m[n]; ok {
-			ng = v
+	res := make([]int, len(nums1))
+	for i := range res {
+		if v, ok := m[nums1[i]]; ok {
+			res[i] = v
+		} else {
+			res[i] = -1
 		}
-		res = append(res, ng)
 	}
+
 	return res
 }
