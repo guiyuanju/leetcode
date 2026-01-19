d9b514f959c5a94f4b3fc74a80d97bf4a57dfe55
guiyuanju <guiyuanjune@outlook.com>
2026-01-19 14:13:37 +0800

    upd 11

--- go/11/main.go
+++ go/11/main.go
@@ -11,17 +11,18 @@ func main() {
 }
 
 func maxArea(height []int) int {
+	var res int
 	i := 0
 	j := len(height) - 1
-	var res int
 	for i < j {
-		res = max(res, min(height[i], height[j])*(j-i))
+		res = max(res, (j-i)*min(height[i], height[j]))
 		if height[i] < height[j] {
 			i++
 		} else {
 			j--
 		}
 	}
+
 	return res
 }
 
