6123595ce96ff677422ee0caa1fe3ca6f1efce45
guiyuanju <guiyuanjune@outlook.com>
2026-01-27 11:34:48 +0800

    upd 474

--- go/474/main.go
+++ go/474/main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	assertEq(4, findMaxForm([]string{"10", "0001", "111001", "1", "0"}, 5, 3))
 	assertEq(2, findMaxForm([]string{"10", "0", "1"}, 1, 1))
+	assertEq(3, findMaxForm([]string{"10", "0001", "111001", "1", "0"}, 4, 3))
 }
 
 func assertEq(a, b any) {
@@ -24,27 +25,35 @@ func findMaxForm(strs []string, m int, n int) int {
 }
 
 func findMaxForm_td(strs []string, m int, n int) int {
-	count := make([][2]int, len(strs))
+	strs01 := make([][2]int, len(strs))
 	for i, s := range strs {
 		for _, c := range s {
 			if c == '0' {
-				count[i][0]++
+				strs01[i][0]++
 			} else {
-				count[i][1]++
+				strs01[i][1]++
 			}
 		}
 	}
 
+	memo := map[[3]int]int{}
+
 	var dp func(i, m, n int) int
 	dp = func(i, m, n int) int {
-		if i >= len(strs) || (m == 0 && n == 0) {
+		if v, ok := memo[[3]int{i, m, n}]; ok {
+			return v
+		}
+
+		if i >= len(strs) {
 			return 0
 		}
 
 		res := dp(i+1, m, n)
-		if count[i][0] <= m && count[i][1] <= n {
-			res = max(res, 1+dp(i+1, m-count[i][0], n-count[i][1]))
+		if m >= strs01[i][0] && n >= strs01[i][1] {
+			res = max(1+dp(i+1, m-strs01[i][0], n-strs01[i][1]), res)
 		}
+
+		memo[[3]int{i, m, n}] = res
 
 		return res
 	}
@@ -53,13 +62,13 @@ func findMaxForm_td(strs []string, m int, n int) int {
 }
 
 func findMaxForm_bu(strs []string, m int, n int) int {
-	count := make([][2]int, len(strs))
+	strs01 := make([][2]int, len(strs))
 	for i, s := range strs {
 		for _, c := range s {
 			if c == '0' {
-				count[i][0]++
+				strs01[i][0]++
 			} else {
-				count[i][1]++
+				strs01[i][1]++
 			}
 		}
 	}
@@ -72,12 +81,12 @@ func findMaxForm_bu(strs []string, m int, n int) int {
 		}
 	}
 
-	for i := len(strs) - 1; i >= 0; i-- {
-		for j := range m + 1 {
-			for k := range n + 1 {
+	for i := len(dp) - 2; i >= 0; i-- {
+		for j := 0; j < len(dp[i]); j++ {
+			for k := 0; k < len(dp[i][j]); k++ {
 				dp[i][j][k] = dp[i+1][j][k]
-				if count[i][0] <= j && count[i][1] <= k {
-					dp[i][j][k] = max(dp[i][j][k], 1+dp[i+1][j-count[i][0]][k-count[i][1]])
+				if j >= strs01[i][0] && k >= strs01[i][1] {
+					dp[i][j][k] = max(dp[i][j][k], 1+dp[i+1][j-strs01[i][0]][k-strs01[i][1]])
 				}
 			}
 		}
