2d6a2804a4a3a5b4d643e14c7a87b5437e69528f
guiyuanju <guiyuanjune@outlook.com>
2026-02-01 10:37:47 +0800

    upd 77

--- go/77/main.go
+++ go/77/main.go
@@ -9,21 +9,24 @@ func main() {
 
 func combine(n int, k int) [][]int {
 	var res [][]int
-	var dp func(i int, cur []int)
-	dp = func(i int, cur []int) {
+	var bt func(i int, cur []int)
+	bt = func(i int, cur []int) {
 		if len(cur) == k {
 			tmp := make([]int, k)
 			copy(tmp, cur)
 			res = append(res, tmp)
 			return
 		}
+		if i > n {
+			return
+		}
 
 		for j := i; j <= n; j++ {
-			dp(j+1, append(cur, j))
+			bt(j+1, append(cur, j))
 		}
 	}
 
-	dp(1, nil)
+	bt(1, nil)
 
 	return res
 }
