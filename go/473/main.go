61dd7f9e1e824780253d65ccef89eda92544282a
guiyuanju <guiyuanjune@outlook.com>
2026-02-08 17:01:48 +0800

    upd

--- go/473/main.go
+++ go/473/main.go
@@ -20,26 +20,29 @@ func assertEq(a, b any) {
 	}
 }
 
+// Time complexity = O(n * L ^ 4) where L = the max value of a conf[i] = totalSum / 4
+// !!! There is a better solution, which is only N * 2 ^ N
 func makesquare(matchsticks []int) bool {
-	var sum int
-	for _, l := range matchsticks {
-		sum += l
+	var lenSum int
+	for _, s := range matchsticks {
+		lenSum += s
 	}
-	if sum%4 != 0 {
+
+	if lenSum%4 != 0 {
 		return false
 	}
-	width := sum / 4
 
-	type state struct {
-		i    int
-		conf [4]int
-	}
-	memo := map[state]bool{}
+	edgeLen := lenSum / 4
+
+	memo := map[[5]int]bool{}
 
 	var dp func(i int, conf [4]int) bool
 	dp = func(i int, conf [4]int) bool {
 		slices.Sort(conf[:])
-		if v, ok := memo[state{i, conf}]; ok {
+
+		key := [5]int{i, conf[0], conf[1], conf[2], conf[3]}
+
+		if v, ok := memo[key]; ok {
 			return v
 		}
 
@@ -47,23 +50,68 @@ func makesquare(matchsticks []int) bool {
 			return true
 		}
 
-		for j, c := range conf {
-			if c >= matchsticks[i] {
-				conf[j] -= matchsticks[i]
+		for j := range conf {
+			if conf[j]+matchsticks[i] <= edgeLen {
+				conf[j] += matchsticks[i]
 				if dp(i+1, conf) {
-					memo[state{i, conf}] = true
+					memo[key] = true
 					return true
 				}
-				conf[j] += matchsticks[i]
+				conf[j] -= matchsticks[i]
 			}
 		}
 
-		memo[state{i, conf}] = false
+		memo[key] = false
 		return false
 	}
 
-	return dp(0, [4]int{width, width, width, width})
+	return dp(0, [4]int{0, 0, 0, 0})
 }
+
+// func makesquare(matchsticks []int) bool {
+// 	var sum int
+// 	for _, l := range matchsticks {
+// 		sum += l
+// 	}
+// 	if sum%4 != 0 {
+// 		return false
+// 	}
+// 	width := sum / 4
+//
+// 	type state struct {
+// 		i    int
+// 		conf [4]int
+// 	}
+// 	memo := map[state]bool{}
+//
+// 	var dp func(i int, conf [4]int) bool
+// 	dp = func(i int, conf [4]int) bool {
+// 		slices.Sort(conf[:])
+// 		if v, ok := memo[state{i, conf}]; ok {
+// 			return v
+// 		}
+//
+// 		if i >= len(matchsticks) {
+// 			return true
+// 		}
+//
+// 		for j, c := range conf {
+// 			if c >= matchsticks[i] {
+// 				conf[j] -= matchsticks[i]
+// 				if dp(i+1, conf) {
+// 					memo[state{i, conf}] = true
+// 					return true
+// 				}
+// 				conf[j] += matchsticks[i]
+// 			}
+// 		}
+//
+// 		memo[state{i, conf}] = false
+// 		return false
+// 	}
+//
+// 	return dp(0, [4]int{width, width, width, width})
+// }
 
 // func makesquare(matchsticks []int) bool {
 // 	var sum int
