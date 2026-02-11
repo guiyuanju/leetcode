da406754e02cace96410b920fad3dbd7632867e5
guiyuanju <guiyuanjune@outlook.com>
2026-02-11 15:31:37 +0800

    upd 2104

--- go/2104/main.go
+++ go/2104/main.go
@@ -1,47 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println(subArrayRanges([]int{1, 2, 3}))
-	fmt.Println(subArrayRanges([]int{1, 3, 3}))
-	fmt.Println(subArrayRanges([]int{4, -2, -3, 4, 1}))
+	assertEq(int64(4), subArrayRanges([]int{1, 2, 3}))
+	assertEq(int64(4), subArrayRanges([]int{1, 3, 3}))
+	assertEq(int64(59), subArrayRanges([]int{4, -2, -3, 4, 1}))
+}
+
+func assertEq(a, b any) {
+	if reflect.DeepEqual(a, b) {
+		fmt.Printf("Ok: %v\n", a)
+	} else {
+		fmt.Printf("Failed: %v != %v\n", a, b)
+	}
 }
 
 func subArrayRanges(nums []int) int64 {
-	monoMin := []int{}
-	monoMax := []int{}
 	mins := make([]int, len(nums))
 	maxs := make([]int, len(nums))
+	monoMins := []int{}
+	monoMaxs := []int{}
 
 	for i, n := range nums {
-		for len(monoMin) > 0 && n < nums[monoMin[len(monoMin)-1]] {
-			monoMin = monoMin[:len(monoMin)-1]
+		for len(monoMins) > 0 && n < nums[monoMins[len(monoMins)-1]] {
+			monoMins = monoMins[:len(monoMins)-1]
 		}
-		monoMin = append(monoMin, i)
-		if len(monoMin) == 1 {
+		monoMins = append(monoMins, i)
+		if len(monoMins) == 1 {
 			mins[i] = n * (i + 1)
 		} else {
-			prev := monoMin[len(monoMin)-2]
-			mins[i] = mins[prev] + n*(i-prev)
+			prevIdx := monoMins[len(monoMins)-2]
+			mins[i] = mins[prevIdx] + n*(i-prevIdx)
 		}
 
-		for len(monoMax) > 0 && n > nums[monoMax[len(monoMax)-1]] {
-			monoMax = monoMax[:len(monoMax)-1]
+		for len(monoMaxs) > 0 && n > nums[monoMaxs[len(monoMaxs)-1]] {
+			monoMaxs = monoMaxs[:len(monoMaxs)-1]
 		}
-		monoMax = append(monoMax, i)
-		if len(monoMax) == 1 {
+		monoMaxs = append(monoMaxs, i)
+		if len(monoMaxs) == 1 {
 			maxs[i] = n * (i + 1)
 		} else {
-			prev := monoMax[len(monoMax)-2]
-			maxs[i] = maxs[prev] + n*(i-prev)
+			prevIdx := monoMaxs[len(monoMaxs)-2]
+			maxs[i] = maxs[prevIdx] + n*(i-prevIdx)
 		}
 	}
 
-	var resMin, resMax int64
+	var res int64
 	for i := range mins {
-		resMin += int64(mins[i])
-		resMax += int64(maxs[i])
+		res += int64(maxs[i]) - int64(mins[i])
 	}
-	return resMax - resMin
+
+	return res
 }
