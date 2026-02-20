93ed60566a05d707b0f5f1f2c101255e41aa6b5c
guiyuanju <guiyuanjune@outlook.com>
2026-02-20 15:26:44 +0800

    upd 523

--- go/523/main.go
+++ go/523/main.go
@@ -15,20 +15,17 @@ func main() {
 }
 
 func checkSubarraySum(nums []int, k int) bool {
-	// the end index (exclusive) of prefix sum
-	ends := map[int]int{}
-	ends[0] = 0
 	var cur int
+	sum := map[int]int{}
+	sum[0] = -1
 	for i, n := range nums {
 		cur += n
-		// (cur - x) % k == 0; cur%k -x%k == 0; x%k == cur%k
-		if end, ok := ends[cur%k]; ok {
-			if i+1-end >= 2 {
+		if idx, ok := sum[cur%k]; ok {
+			if i-idx >= 2 {
 				return true
 			}
 		} else {
-			// store the smallest index, satisfy the length greedy
-			ends[cur%k] = i + 1
+			sum[cur%k] = i
 		}
 	}
 	return false
