42ba16f4337979166e231cebc17f1d0794629d3f
guiyuanju <guiyuanjune@outlook.com>
2026-03-01 14:30:06 +0800

    upd 2402

--- go/2402/main.go
+++ go/2402/main.go
@@ -22,8 +22,6 @@ func main() {
 	n = 4
 	meetings = [][]int{{18, 19}, {3, 12}, {17, 19}, {2, 13}, {7, 10}}
 	assertEq(0, mostBooked(n, meetings))
-	// [2, 13, 0] [3, 12, 1] [7, 10, 2] [17, 19, 3]
-	// 1 1 1 1
 
 	n = 3
 	meetings = [][]int{{44, 50}, {22, 37}, {46, 49}, {35, 45}, {11, 21}, {31, 32}, {16, 45}}
@@ -40,38 +38,40 @@ func assertEq[T comparable](a, b T) {
 
 func mostBooked(n int, meetings [][]int) int {
 	slices.SortFunc(meetings, func(a, b []int) int { return a[0] - b[0] })
-	count := make([]int, n)
-	using := Heap{}
-	empty := Heap{}
+	empty := Heap([]Ele{})
 	for i := range n {
 		empty = append(empty, Ele{0, i})
 	}
+	heap.Init(&empty)
+	using := Heap([]Ele{})
+	count := make([]int, n)
 	for _, m := range meetings {
-		for len(using) > 0 && m[0] >= using[0].end {
+		for len(using) > 0 && using[0].end <= m[0] {
 			room := heap.Pop(&using).(Ele)
 			room.end = 0
 			heap.Push(&empty, room)
 		}
-		var room Ele
-		if (len(empty)) == 0 {
-			room = heap.Pop(&using).(Ele)
-			room.end += m[1] - m[0]
+		var target Ele
+		if len(empty) == 0 {
+			target = heap.Pop(&using).(Ele)
+			target.end += m[1] - m[0]
 		} else {
-			room = heap.Pop(&empty).(Ele)
-			room.end = m[1]
+			target = heap.Pop(&empty).(Ele)
+			target.end = m[1]
 		}
-		heap.Push(&using, room)
-		count[room.num]++
+		count[target.num]++
+		heap.Push(&using, target)
 	}
 
-	var hi, idx int
-	for i, n := range count {
-		if n > hi {
-			hi = n
-			idx = i
+	var res, hi int
+	for i, c := range count {
+		if c > hi {
+			hi = c
+			res = i
 		}
 	}
-	return idx
+
+	return res
 }
 
 type Ele struct {
