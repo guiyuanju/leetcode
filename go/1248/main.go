fe12d6a7d5f210a4ff7ea35e288e89029f42ab5a
guiyuanju <guiyuanjune@outlook.com>
2026-03-19 10:56:57 +0800

    upd 1248

--- go/1248/main.go
+++ go/1248/main.go
@@ -13,7 +13,7 @@ func main() {
 func numberOfSubarrays(nums []int, k int) int {
 	count := map[int]int{}
 	count[0] = 1
-	var res, cur int
+	var cur, res int
 	for _, n := range nums {
 		if n%2 == 1 {
 			cur++
